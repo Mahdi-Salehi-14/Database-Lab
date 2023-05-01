1be7c608011f6990dce02e3aea241a831b677750
Mahdi-Salehi-14 <Mahdi2015GAD@Gmail.com>
2023-05-01 21:56:32 +0330

    Week-4

--- Week-4/Week-3-SelectProduct.php
+++ Week-4/Week-3-SelectProduct.php
@@ -3,6 +3,7 @@ include 'DataBase.php';
 include 'Settings.php';
 $db = new db($dbhost, $dbuser, $dbpass, $dbname);
 $sql = "SELECT * FROM product
+        WHERE brand = 'nike'
         ORDER BY productprice";
 $result = $db -> query($sql);
 $productlist = $result -> fetchAll();
